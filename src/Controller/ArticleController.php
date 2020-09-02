a6a9fc40ddafddf4c386a248f26c6ad6b655a61e
Golgot <xribant@gmail.com>
2020-09-02 10:12:43 +0200

    Making so much good progress

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,11 +3,29 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class ArticleController
 {
+
+	/**
+     * @Route("/")
+     */
+
     public function homepage()
     {
         return new Response('OMG! My first page already! WOOO!');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+
+    public function show($slug)
+    {
+    	return new Response(sprintf(
+            'Future page to show the article: "%s"',
+            $slug
+        ));
     }
 }
